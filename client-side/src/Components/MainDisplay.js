a6600eda073b9556d566aac67359b8e9de52f846
Johnstone Marucha <marucha.webdev@yahoo.com>
2022-11-03 16:31:37 +0300

    Configure Frontend

---
+++ client-side/src/Components/MainDisplay.js
@@ -0,0 +1,11 @@
+import { useEffect, useState, useRef } from 'react'
+
+function MainDisplay(){ 
+    return (
+        <>
+            <h2>All Components Go here </h2>
+        </>
+    )
+}
+
+export default MainDisplay;
